af975281891a787b2bdeb18fbadc93283c326f2f
Eli Jensen <eli.mit.jensen@gmail.com>
2024-09-01 10:48:53 -0400

    Different home page

---
+++ app/options/page.tsx
@@ -0,0 +1,12 @@
+import RunningMan from '../components/RunningMan';
+import Bunny from '../components/Bunny';
+
+export default function Home() {
+  return (
+    <main>
+      <Bunny />
+      <RunningMan />
+      {/* You can add more content here */}
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-// app/page.tsx
-import RunningMan from './components/RunningMan';
-import Bunny from './components/Bunny';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
-      <Bunny />
-      <RunningMan />
-      {/* You can add more content here */}
-    </main>
+    <div style={{ backgroundColor: 'black', height: '100vh' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '20px' }}>
+        <Link href="/options">
+          <div style={{ maxWidth: '30vw', maxHeight: '90vh', width: '100%', height: 'auto' }}>
+            <Image
+              src="/images/self-portrait.jpg"
+              alt="Self Portrait"
+              width={5433}
+              height={6496}
+              layout="responsive"
+              style={{ cursor: 'pointer' }}
+            />
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 }
