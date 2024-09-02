b90e448f851986dc97bcf9fa8d66455430c24d3d
Eli Jensen <eli.mit.jensen@gmail.com>
2024-09-02 13:55:32 -0400

    Fix viewport

--- app/bio-contact/page.tsx
+++ app/bio-contact/page.tsx
@@ -5,12 +5,12 @@ import BackToCategoriesButton from '../components/BackToCategoriesButton';
 
 export default function BioContact() {
   return (
-    <div>
+    <div style={{ minHeight: '100vh'}}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px' }}>
         <Link href="/categories">
           <BackToCategoriesButton />
         </Link>
-        <h1 style={{ textAlign: 'center', flex: '1', fontSize: '2rem' }}>"Bio and contact goes here"</h1>
+        <h1 style={{ textAlign: 'center', flex: '1', fontSize: '2rem' }}>Bio and contact goes here</h1>
       </div>
     </div>
   )
