3828102d5a674bb2f1f1b7f140af0e9fddb6437d
Eli Jensen <eli.mit.jensen@gmail.com>
2024-09-08 17:45:37 -0400

    Bio placeholder

--- app/bio-contact/page.tsx
+++ app/bio-contact/page.tsx
@@ -5,13 +5,24 @@ import BackToCategoriesButton from '../components/BackToCategoriesButton';
 
 export default function BioContact() {
   return (
-    <div style={{ minHeight: '100vh'}}>
+    <div style={{ minHeight: '100vh' }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px' }}>
         <Link href="/categories">
           <BackToCategoriesButton />
         </Link>
-        <h1 style={{ textAlign: 'center', flex: '1', fontSize: '2rem' }}>Bio and contact goes here</h1>
+        <h1 style={{ textAlign: 'center', flex: '1', fontSize: '2rem' }}>Bio and Contact</h1>
+      </div>
+      <div style={{ padding: '20px', textAlign: 'left', maxWidth: '800px', margin: '0 auto' }}>
+        <p>
+          ----- Bio placeholder -----
+        </p>
+        <p>
+          <strong>Ada Mitrani</strong> is a Bulgarian American artist.
+        </p>
+        <p>
+          Contact: -----
+        </p>
       </div>
     </div>
-  )
-};
+  );
+}
